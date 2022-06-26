43fbfe9a713ab984fd5718b26b3e7b4778b2805b
Farid <61904148+Farid1328@users.noreply.github.com>
2022-06-26 18:59:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from replit import clear
+import blindauction
+print('Welcome to blind auction')
+print(blindauction.logo)
+blind = {}
+while True:
+  name = input('Enter Your name? ') 
+  bid = int(input('Enter Your bid '))
+  blind.update({name:bid})
+  other = input('Is the any other bidders')
+  if other == 'yes':
+    clear()
+    continue
+  else:
+    x = 0
+    for value in blind:
+      if blind[value] > x:
+        x+=blind[value]
+    print('the higts bid',x)
+       
+        
+  
+    
+
+
+
+
